30089ea08b641b89ef8e9d2bd28d7ad919464b5c
Alexander Selyuchenko <alexander.selyuchenko@gmail.com>
2020-05-21 19:55:31 +0300

    Fix a typo

--- lang/ru/install/index.php
+++ lang/ru/install/index.php
@@ -1,5 +1,5 @@
 <?php
 $MESS['WJS_MODULE_NAME'] = "GraphQL API";
-$MESS['WJS_INSTALL_DESCRIPTION'] = "Многоцелевое расширяемое API на GraphQL";
+$MESS['WJS_INSTALL_DESCRIPTION'] = "Многоцелевой расширяемый API на GraphQL";
 $MESS['WJS_INSTALL_TITLE'] = "Установка модуля";
 $MESS['WJS_UNINSTALL_TITLE'] = "Удаление модуля";
